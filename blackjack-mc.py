709c49aff3a8c05d0b2f578ac11f3a9f3f8449be
Vladimir Valeyev <ValV@users.noreply.github.com>
2024-12-21 00:20:00 +0300

    Implement incremental updates (incremental implementation)

--- blackjack-mc.py
+++ blackjack-mc.py
@@ -45,7 +45,7 @@ class MonteCarloControl:
         )  # tracks returns for each state-action pair
         self.policy = defaultdict(
             lambda: np.random.choice(self.env.action_space.n)
-        )
+        )  # exploring starts
 
     def generate_episode(self):
         """Generate an episode by following the stored policy."""
@@ -94,6 +94,76 @@ class MonteCarloControl:
 
             # Optional: print progress
             if (i + 1) % 1000 == 0:
+                print(f"Episode {i + 1}/{episodes} completed.")
+
+        print("Training completed!")
+
+
+# Monte Carlo Control with Exploring Starts (incremental implementation)
+class MonteCarloControlIncremental:
+    def __init__(self, env, gamma=1.0, epsilon=0.1, progress_steps=1000):
+        self.env = env
+        self.gamma = gamma  # discount factor
+        self.epsilon = epsilon  # epsilon-greedy strategy factor
+        self.Q = defaultdict(
+            lambda: np.zeros(env.action_space.n, dtype=float)
+        )  # state-action value
+        self.N = defaultdict(
+            lambda: np.zeros(env.action_space.n, dtype=int)
+        )  # tracks returns for each state-action pair
+        self.policy = defaultdict(
+            lambda: np.random.choice(self.env.action_space.n)
+        )  # exploring starts
+        self.progress_steps = progress_steps
+
+    def generate_episode(self):
+        """Generate an episode by following the stored policy."""
+        episode = []
+        state = self.env.reset()[0]
+        while True:
+            # Use epsilon-greedy policy to explore
+            action = int(
+                self.policy[state]
+                # if state in self.policy
+                if np.random.random() >= self.epsilon
+                else np.random.choice(self.env.action_space.n)
+            )
+            next_state, reward, done, _, _ = self.env.step(action)
+            episode.append((state, action, reward))
+            if done:
+                break
+            state = next_state
+        return episode
+
+    def update_policy(self):
+        """Update the policy to be greedy with respect to Q."""
+        for state in self.Q:
+            # Select the action with the maximum Q-value for the state
+            best_action = int(np.argmax(self.Q[state]))
+            self.policy[state] = best_action
+
+    def train(self, episodes=50000):
+        """Train the policy using Monte Carlo control."""
+        for i in range(episodes):
+            episode = self.generate_episode()  # policy evaluation
+            G = 0  # initialize return
+            visited = set()  # keep track of visited state-action pairs
+
+            # Process episode in reverse for calculating returns
+            for state, action, reward in reversed(episode):
+                G = self.gamma * G + reward
+                if (state, action) not in visited:
+                    visited.add((state, action))
+                    # Implement statistics here
+                    self.N[state][action] += 1  # increment count
+                    alpha = 1.0 / self.N[state][action]  # step-size
+                    self.Q[state][action] += alpha * (
+                        G - self.Q[state][action]
+                    )  # incremental update
+            self.update_policy()  # policy improvement
+
+            # Optional: print progress
+            if (i + 1) % self.progress_steps == 0:
                 print(f"Episode {i + 1}/{episodes} completed.")
 
         print("Training completed!")
@@ -306,13 +376,16 @@ def plot_policy_grid(ax, x_vals, y_vals, policy_grid, title):
 
 
 agents = []
-iterations = 5
+instances = 5
 
 # Train multiple agents with different initialization
-for _ in range(iterations):
+for k in range(instances):
     # Initialize and train
-    mc_control = MonteCarloControl(env, epsilon=0.4)
-    mc_control.train(episodes=1000000)
+    print(f"Start training instance {k:02d}")
+    mc_control = MonteCarloControlIncremental(
+        env, epsilon=0.3, progress_steps=10000
+    )
+    mc_control.train(episodes=10000000)
     agents.append(mc_control)
 
 # Test the trained policy
